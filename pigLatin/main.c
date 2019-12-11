a82c4d1ba8e87caf20d024747229d7f7f54c6e99
Aidan Rosen <aidanrosen04@gmail.com>
2019-12-11 13:55:19 -0800

    Initial Commit

---
+++ pigLatin/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pigLatin
+//
+//  Created by Rosen, Aidan on 12/11/19.
+//  Copyright © 2019 Rosen, Aidan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
