dfc834daf32dbd76c87abd34ad77c7ff4d37481a
Aidan Rosen <aidanrosen04@gmail.com>
2020-01-06 14:26:42 -0800

    First commit <3

--- tokenExperiment/main.c
+++ tokenExperiment/main.c
@@ -5,11 +5,11 @@
 //  Created by Rosen, Aidan on 12/16/19.
 //  Copyright © 2019 Rosen, Aidan. All rights reserved.
 //
-
+#include "tokenizer.h"
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    tokenize(); 
     return 0;
 }
