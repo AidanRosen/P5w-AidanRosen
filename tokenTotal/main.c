c0924be7690c2c8097f71667f0fcd3735297bc26
Aidan Rosen <aidanrosen04@gmail.com>
2020-01-07 15:00:37 -0800

    Initial Commit

---
+++ tokenTotal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tokenTotal
+//
+//  Created by Rosen, Aidan on 1/7/20.
+//  Copyright © 2020 Rosen, Aidan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
