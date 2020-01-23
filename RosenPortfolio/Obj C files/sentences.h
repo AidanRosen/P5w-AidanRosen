621bcfbb42dc8854daed584283374728dcb69e16
Aidan Rosen <aidanrosen04@gmail.com>
2020-01-23 12:55:10 -0800

    All things fixed

---
+++ RosenPortfolio/Obj C files/sentences.h
@@ -0,0 +1,20 @@
+//
+//  sentences.h
+//  RosenPortfolio
+//
+//  Created by Rosen, Aidan on 1/21/20.
+//  Copyright © 2020 Rosen, Aidan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface stringManipulator : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *input2;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UILabel *latinWay;
+@property (weak, nonatomic) IBOutlet UILabel *reversedWay;
+@property (weak, nonatomic) IBOutlet UILabel *shortWay;
+
+
+@end
+
+
